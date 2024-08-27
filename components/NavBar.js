96a617febe4ff207bbb3c9f36de8e79f9a1748e6
Melissa Carter <mkd.princess@gmail.com>
2024-08-27 13:33:03 -0500

    added nav buttons

--- components/NavBar.js
+++ components/NavBar.js
@@ -16,9 +16,11 @@ export default function NavBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>Dashboard</Nav.Link>
+            </Link>
+            <Link passHref href="/">
+              <Nav.Link>Visits</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
