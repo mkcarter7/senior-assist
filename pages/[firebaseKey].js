984225245272ddb9b9dba08f9efa599d486c3c9e
Melissa Carter <mkd.princess@gmail.com>
2024-08-27 21:35:18 -0500

    dynamic route

---
+++ pages/[firebaseKey].js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+import { useRouter } from 'next/router';
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const router = useRouter();
+const { firebaseKey } = router.query;
